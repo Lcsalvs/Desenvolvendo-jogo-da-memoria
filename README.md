30c2a8e121b2a172772aa5ed77293b52c8d5923a
Lucas Alves da Conceição <lcsalves1999@gmail.com>
2020-09-15 12:51:45 -0300

    Adicionando últimas funções do jogo
    Adicionando últimas funções do jogo e finalizando o projeto.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Desenvolvendo-jogo-da-memoria
  Desenvolvendo um jogo da memória com HTML, CSS e JS.
+
+ Projeto feito na plataforma da Digital Innovation One no bootcamp JS Game Developer.
--- script.js
+++ script.js
@@ -1,9 +1,71 @@
-const card = document.querySelector('.card')
+const cards = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard, secondCard
+let lockBoard = false
 
+//Função para virar a carta
 function flipCard(){
-    this.classList.toggle('flip')
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip')
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
 }
 
+//Função que checa se as cartas são iguais
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+//Função que desabilita as cartas
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//Função que desvira as cartas
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout (() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500)
+}
+
+//Função que reseta o tabuleiro
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//Função que embaralha as cartas
+(function shuffle(){
+    window.alert("Welcome to the game!")
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
+//Adiciona evento de click na carta
 cards.forEach((card) => {
-    card.addEventListener('click', flipCard)
+    card.addEventListener('click', flipCard);
 })
